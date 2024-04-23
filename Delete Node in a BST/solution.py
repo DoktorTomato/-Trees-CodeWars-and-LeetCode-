a19254eed69d197ebf2a0e7e1d244dd59b3323cf
DoktorTomato <96630602+DoktorTomato@users.noreply.github.com>
2024-04-23 17:23:56 +0300

    New version of "Delete node in BST" that works better

--- Delete Node in a BST/solution.py
+++ Delete Node in a BST/solution.py
@@ -1,51 +1,53 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return f'{self.val} l: {self.left} | r: {self.right}'
+
 class Solution:
     def deleteNode(self, root, key: int):
-        cur = root
-        to_del = None
         if root is None:
             return root
-        if (root.val == key and
-            root.left is None and
-            root.right is None):
-            return None
-        while True:
-            if cur is None:
-                return root
-            if cur.val == key:
-                to_del = cur
-                break
-            elif cur.val < key:
-                cur = cur.right
-            elif cur.val > key:
-                cur = cur.left
-        if (to_del.left is None and
-            to_del.right is None):
-            to_del = None
-        elif to_del.left is None:
-            to_del.val = to_del.left.val
-            to_del.right = to_del.left.right
-            to_del.left = to_del.left.left
-        elif to_del.right is None:
-            to_del.val = to_del.right.val
-            to_del.left = to_del.right.left
-            to_del.right = to_del.right.right
-        else:
-            prev = to_del
-            change_to = to_del.right
-            count = 0
-            while change_to.left:
-                prev = change_to
-                change_to = prev.left
-                count += 1
-            to_del.val = change_to.val
-            if count == 0:
-                prev.right = None
+        if root.val == key:
+            if (root.left is None and
+                root.right is None):
+                root = None
+            elif root.left is None:
+                root.val = root.right.val
+                root.left = root.right.left
+                root.right = root.right.right
+            elif root.right is None:
+                root.val = root.left.val
+                root.right = root.left.right
+                root.left = root.left.left
             else:
-                prev.left = None
-        return root
+                prev = root
+                change_to = root.right
+                count = 0
+                while change_to.left:
+                    prev = change_to
+                    change_to = change_to.left
+                    count += 1
+                root.val = change_to.val
+                if count == 0:
+                    prev.right = None
+                    root.right = change_to.right
+                else:
+                    prev.left = None
+            return root
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+            return root
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+            return root
+
+if __name__ == '__main__':
+    sol = Solution()
+    tree = TreeNode(50, TreeNode(30, None, TreeNode(40)), TreeNode(70, TreeNode(60),TreeNode(80)))
+    sol.deleteNode(tree, 50)
+    print(tree)
